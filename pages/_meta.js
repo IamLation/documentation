f3cbdd207b270fdb80821c7a0525f27d0a487173
Lation <lationj@pm.me>
2025-02-03 17:28:43 -0500

    feat(chopshop): add chopshop section

--- pages/_meta.js
+++ pages/_meta.js
@@ -1,5 +1,6 @@
 export default {
   index: "Introduction",
   lation_weed: "Advanced Weed Growing",
-  lation_meth: "Advanced Meth Crafting"
+  lation_meth: "Advanced Meth Crafting",
+  lation_chopshop: "Advanced Chop Shop"
 };
