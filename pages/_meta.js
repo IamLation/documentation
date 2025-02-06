9f153c2f12d6c6b7136f00a2e8de239d2ec34820
Lation <lationj@pm.me>
2025-02-06 16:00:20 -0500

    feat(laundering): complete money laundering section

--- pages/_meta.js
+++ pages/_meta.js
@@ -1,6 +1,7 @@
 export default {
   index: "Introduction",
-  lation_weed: "Advanced Weed Growing",
-  lation_meth: "Advanced Meth Crafting",
-  lation_chopshop: "Advanced Chop Shop"
+  lation_weed: "Weed Growing",
+  lation_meth: "Meth Crafting",
+  lation_chopshop: "Chop Shop",
+  lation_laundering: "Money Laundering",
 };
