57594ebda6d8e7aad53c7b3129913816fb6bc6b2
Lation <lationj@pm.me>
2025-02-18 09:20:53 -0500

    chore: remove vercel dependencies for self host preperation

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,13 +1,9 @@
 import 'nextra-theme-docs/style.css';
-import { Analytics } from '@vercel/analytics/react';
-import { SpeedInsights } from '@vercel/speed-insights/react';
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <>
       <Component {...pageProps} />
-      <Analytics />
-      <SpeedInsights />
     </>
   );
 }
