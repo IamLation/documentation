d683c39a7918e2fa6e9e5604d1dff4e9bc52d958
Lation <lationj@pm.me>
2025-02-18 19:00:24 -0500

    feat: add GA analytics

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,8 +1,29 @@
 import 'nextra-theme-docs/style.css';
+import Script from 'next/script';
+
+const GA_MEASUREMENT_ID = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID;
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <>
+      {GA_MEASUREMENT_ID && (
+        <>
+          <Script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+            strategy="afterInteractive"
+          />
+          <Script id="google-analytics" strategy="afterInteractive">
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${GA_MEASUREMENT_ID}');
+            `}
+          </Script>
+        </>
+      )}
+
       <Component {...pageProps} />
     </>
   );
