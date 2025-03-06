e7ee23cf9534a39fb341a4f2b9d8a5b37d2768e9
Lation <lationj@pm.me>
2025-03-06 14:04:07 -0500

    feat: add microsoft clarity

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -4,10 +4,12 @@ import { Analytics } from '@vercel/analytics/react';
 import { SpeedInsights } from '@vercel/speed-insights/react';
 
 const GA_MEASUREMENT_ID = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID;
+const CLARITY_ID = process.env.NEXT_PUBLIC_CLARITY_ID;
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <>
+      {/* Google Analytics */}
       {GA_MEASUREMENT_ID && (
         <>
           <Script
@@ -24,6 +26,19 @@ export default function MyApp({ Component, pageProps }) {
             `}
           </Script>
         </>
+      )}
+
+      {/* Microsoft Clarity */}
+      {CLARITY_ID && (
+        <Script id="microsoft-clarity" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "${CLARITY_ID}");
+          `}
+        </Script>
       )}
 
       <Component {...pageProps} />
