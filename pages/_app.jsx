e0f2673f267e06ed37c9087a4a8bb010a0bd0782
Lation <lationj@pm.me>
2025-03-06 14:39:58 -0500

    fix: try useEffect for clarity

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,5 +1,6 @@
 import 'nextra-theme-docs/style.css';
 import Script from 'next/script';
+import { useEffect } from 'react';
 import { Analytics } from '@vercel/analytics/react';
 import { SpeedInsights } from '@vercel/speed-insights/react';
 
@@ -7,6 +8,16 @@ const GA_MEASUREMENT_ID = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID;
 const CLARITY_ID = process.env.NEXT_PUBLIC_CLARITY_ID;
 
 export default function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    if (CLARITY_ID) {
+      (function(c, l, a, r, i, t, y) {
+        c[a] = c[a] || function() { (c[a].q = c[a].q || []).push(arguments) };
+        t = l.createElement(r); t.async = 1; t.src = "https://www.clarity.ms/tag/" + i;
+        y = l.getElementsByTagName(r)[0]; y.parentNode.insertBefore(t, y);
+      })(window, document, "clarity", "script", CLARITY_ID);
+    }
+  }, []);
+
   return (
     <>
       {/* Google Analytics */}
@@ -26,19 +37,6 @@ export default function MyApp({ Component, pageProps }) {
             `}
           </Script>
         </>
-      )}
-
-      {/* Microsoft Clarity */}
-      {CLARITY_ID && (
-        <Script id="microsoft-clarity" strategy="afterInteractive">
-          {`
-            (function(c,l,a,r,i,t,y){
-                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-            })(window, document, "clarity", "script", "${CLARITY_ID}");
-          `}
-        </Script>
       )}
 
       <Component {...pageProps} />
